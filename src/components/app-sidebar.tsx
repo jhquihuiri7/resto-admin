8dac629e271fa33a19d599ad763e160521df2ce5
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2025-03-17 14:38:47 -0600

    dashboard layout updated

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -15,8 +15,6 @@ import {
 } from "lucide-react"
 
 import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavSecondary } from "@/components/nav-secondary"
 import { NavUser } from "@/components/nav-user"
 import {
   Sidebar,
@@ -177,8 +175,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
-        <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
